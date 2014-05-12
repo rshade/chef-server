d5f4bbf3f73892ca3b5b68503f2917ae3884f5b2
Alex Pop <alex@rightscale.com>
2014-05-12 18:31:54 +0100

    add remote_file input

--- cookbook/chef-server-blueprint/metadata.rb
+++ cookbook/chef-server-blueprint/metadata.rb
@@ -13,13 +13,18 @@ depends "chef-server"
   supports os
 end
 
-recipe "chef-server-blueprint::default", "Same as chef-server::default, but with inputs"
+recipe "chef-server-blueprint::default", "Same as chef-server::default, but with inputs and optional package download"
 
 attribute "chef-server-blueprint/api_fqdn",
     :display_name => "Chef Server FQDN",
     :required => "required"
 
 attribute "chef-server-blueprint/version",
-          :display_name => "Chef Server Version",
-          :required => "optional",
-          :default => "latest"
+    :display_name => "Chef Server Version",
+    :required => "optional",
+    :default => "latest"
+    
+attribute "chef-server-blueprint/remote_file",
+    :display_name => "Chef Server URL",
+    :description =>  "Instead of specifying the Chef Server Version, you can provide a URL to a chef-server binary to be downloaded and installed. Ex: http://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-server-11.0.12-1.el6.x86_64.rpm",
+    :required => "optional"
