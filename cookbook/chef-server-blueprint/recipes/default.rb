423d8b5679f6ab91083549af0b8c911b8428a03f
Alex Pop <alex@rightscale.com>
2014-05-13 09:16:51 +0100

    adding -blueprint to the input

--- cookbook/chef-server-blueprint/recipes/default.rb
+++ cookbook/chef-server-blueprint/recipes/default.rb
@@ -12,20 +12,20 @@ rightscale_marker :begin
 node["chef-server"]["api_fqdn"] = node["chef-server-blueprint"]["api_fqdn"]
 node["chef-server"]["version"] = node["chef-server-blueprint"]["version"]
 
-if node['chef-server']['remote_file'].nil? || node['chef-server']['remote_file'].empty?
-  log "*** Input node['chef-server']['remote_file'] is undefined, not setting node['chef-server']['package_file']"
+if node['chef-server-blueprint']['remote_file'].nil? || node['chef-server-blueprint']['remote_file'].empty?
+  log "*** Input node['chef-server-blueprint']['remote_file'] is undefined, not setting node['chef-server']['package_file']"
 else
   filename=""
-  if (node['chef-server']['remote_file'] =~ /.+\/(.+)$/)
+  if (node['chef-server-blueprint']['remote_file'] =~ /.+\/(.+)$/)
     filename=$1
   else
-    throw "*** node['chef-server']['remote_file'] is not a valid URL, aborting..."
+    throw "*** node['chef-server-blueprint']['remote_file'] with value #{node['chef-server-blueprint']['remote_file']} is not a valid URL, aborting..."
   end
   
-  log "*** Downloading #{node['chef-server']['remote_file']} to /root/#{filename}"
+  log "*** Downloading #{node['chef-server-blueprint']['remote_file']} to /root/#{filename}"
 
   remote_file "/root/#{filename}" do
-    source "#{node['chef-server']['remote_file']}"
+    source "#{node['chef-server-blueprint']['remote_file']}"
     action :create_if_missing
   end
   
