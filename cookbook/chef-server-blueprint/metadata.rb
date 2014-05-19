6c12bfe9ddde555972316f6abd442d521843b17b
Alex Pop <alex@rightscale.com>
2014-05-19 12:04:07 +0100

    Update metadata.rb

--- cookbook/chef-server-blueprint/metadata.rb
+++ cookbook/chef-server-blueprint/metadata.rb
@@ -22,16 +22,19 @@ recipe "chef-server-blueprint::backup_schedule_disable", "Disables chef-server-b
 attribute "chef-server-blueprint/api_fqdn",
     :display_name => "Chef Server FQDN",
     :required => "required"
+    :recipes => [ "chef-server-blueprint::default" ]
 
 attribute "chef-server-blueprint/version",
     :display_name => "Chef Server Version",
     :required => "optional",
     :default => "latest"
+    :recipes => [ "chef-server-blueprint::default" ]
     
 attribute "chef-server-blueprint/remote_file",
     :display_name => "Chef Server URL",
     :description =>  "Instead of specifying the Chef Server Version, you can provide a URL to a chef-server binary to be downloaded and installed. Ex: http://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-server-11.0.12-1.el6.x86_64.rpm",
-    :required => "optional"
+    :required => "optional",
+    :recipes => [ "chef-server-blueprint::default" ]
 
 
 # Backup attributes
