8579408032a3d4bb92803dd760cf955588db5f60
Alex Pop <alex@rightscale.com>
2014-05-19 14:26:14 +0100

    Update chef-ros-restore.rb

--- cookbook/chef-server-blueprint/recipes/chef-ros-restore.rb
+++ cookbook/chef-server-blueprint/recipes/chef-ros-restore.rb
@@ -9,16 +9,16 @@ log "*** in recipe: chef-server-blueprint::chef-ros-restore"
 
 raise "*** ROS gem missing, please add rightscale::install_tools recipes to runlist." unless File.exists?("/opt/rightscale/sandbox/bin/ros_util")
 
-if (("#{node[:chef-server-blueprint][:backup][:storage_account_id]}" == "") ||
-    ("#{node[:chef-server-blueprint][:backup][:storage_account_secret]}" == "") ||
-    ("#{node[:chef-server-blueprint][:backup][:container]}" == "") ||
-    ("#{node[:chef-server-blueprint][:backup][:lineage]}" == ""))
+if (("#{node['chef-server-blueprint']['backup']['storage_account_id']}" == "") ||
+    ("#{node['chef-server-blueprint']['backup']['storage_account_secret']}" == "") ||
+    ("#{node['chef-server-blueprint']['backup']['container']}" == "") ||
+    ("#{node['chef-server-blueprint']['backup']['lineage']}" == ""))
   raise "*** Attributes chef-server-blueprint/backup/storage_account_id, storage_account_secret, container and lineage are required by chef-server-blueprint::chef-ros-backup. Aborting"
 end
 
-container = node[:chef-server-blueprint][:backup][:container]
-cloud = node[:chef-server-blueprint][:backup][:storage_account_provider]
-prefix = node[:chef-server-blueprint][:backup][:lineage]
+container = node['chef-server-blueprint']['backup']['container']
+cloud = node['chef-server-blueprint']['backup']['storage_account_provider']
+prefix = node['chef-server-blueprint']['backup']['lineage']
 backup_script = ::File.join(::File.dirname(__FILE__), "..", "files", "default", "chef-backup.sh")
 
 # Overrides default endpoint or for generic storage clouds such as Swift.
@@ -26,18 +26,18 @@ backup_script = ::File.join(::File.dirname(__FILE__), "..", "files", "default", 
 require 'json'
 
 options =
-  if node[:chef-server-blueprint][:backup][:storage_account_endpoint].to_s.empty?
+  if node['chef-server-blueprint']['backup']['storage_account_endpoint'].to_s.empty?
     {}
   else
     {'STORAGE_OPTIONS' => JSON.dump({
-      :endpoint => node[:chef-server-blueprint][:backup][:storage_account_endpoint],
-      :cloud => node[:chef-server-blueprint][:backup][:storage_account_provider].to_sym
+      :endpoint => node['chef-server-blueprint']['backup']['storage_account_endpoint'],
+      :cloud => node['chef-server-blueprint']['backup']['storage_account_provider'].to_sym
     })}
   end
 
 environment_variables = {
-  'STORAGE_ACCOUNT_ID' => node[:chef-server-blueprint][:backup][:storage_account_id],
-  'STORAGE_ACCOUNT_SECRET' => node[:chef-server-blueprint][:backup][:storage_account_secret]
+  'STORAGE_ACCOUNT_ID' => node['chef-server-blueprint']['backup']['storage_account_id'],
+  'STORAGE_ACCOUNT_SECRET' => node['chef-server-blueprint']['backup']['storage_account_secret']
 }.merge(options)
 
 bash "*** Downloading latest backup from '#{container}/chef-backups/', cloud #{cloud}" do
@@ -48,7 +48,7 @@ bash "*** Downloading latest backup from '#{container}/chef-backups/', cloud #{c
     tmp_dir=`mktemp -d`
     /opt/rightscale/sandbox/bin/ros_util get --cloud #{cloud}\
                                              --container #{container}\
-                                             --dest $tmp_dir\
+                                             --dest $tmp_dir/\
                                              --source #{prefix} --latest
     #{backup_script} --restore $tmp_dir/*
   EOH
