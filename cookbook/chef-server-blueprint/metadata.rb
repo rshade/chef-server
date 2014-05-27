0543a3998401c46e9008267326983863a47a4a69
Alex Pop <alex@rightscale.com>
2014-05-27 15:23:36 +0100

    Update metadata.rb

--- cookbook/chef-server-blueprint/metadata.rb
+++ cookbook/chef-server-blueprint/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'me@ryangeyer.com'
 license          'All rights reserved'
 description      'Installs/Configures chef-server-blueprint'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends "rightscale"
 depends "chef-server"
